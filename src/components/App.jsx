5c4b25e24a51b564534510e7c27e348bcfc94a88
Serhii Osmolovskyi <osmolovsky.s@gmail.com>
2024-03-15 12:40:05 -0600

    add loading error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,14 @@ import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import css from './App.module.css';
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchContacts } from 'reduxPhoneBook/operations';
+import { selectError, selectIsLoading } from 'reduxPhoneBook/selectors';
+import { Loader } from './Loader/Loader';
 
 export const App = () => {
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchContacts());
@@ -16,10 +20,11 @@ export const App = () => {
     <div className={css.appContainer}>
       <h1 className={css.title}>Phonebook</h1>
       <ContactForm />
-
       <h2 className={css.subtitle}>Contacts</h2>
       <Filter />
       <ContactList />
+      {isLoading && <Loader />}
+      {error && <p>{error}</p>}
     </div>
   );
 };
--- src/reduxPhoneBook/phoneBookSlice.js
+++ src/reduxPhoneBook/phoneBookSlice.js
@@ -1,6 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { fetchContacts, addContact, deleteContact } from './operations';
 
+const handlePending = (state, { payload }) => {
+  state.contacts.isLoading = true;
+  state.contacts.error = null;
+};
+
+const handleReject = (state, { payload }) => {
+  state.contacts.error = payload;
+  state.contacts.isLoading = false;
+};
+
 const phoneBookSlice = createSlice({
   name: 'phoneBook',
   initialState: {
@@ -25,42 +35,26 @@ const phoneBookSlice = createSlice({
   },
   extraReducers: builder => {
     builder
-      .addCase(fetchContacts.pending, (state, { payload }) => {
-        state.contacts.isLoading = true;
-        state.contacts.error = null;
-      })
+      .addCase(fetchContacts.pending, handlePending)
+      .addCase(fetchContacts.rejected, handleReject)
       .addCase(fetchContacts.fulfilled, (state, { payload }) => {
         state.contacts.items = payload;
         state.contacts.isLoading = false;
       })
-      .addCase(fetchContacts.rejected, (state, { payload }) => {
-        state.contacts.error = payload;
-        state.contacts.isLoading = false;
-      })
-      .addCase(addContact.pending, (state, { payload }) => {
-        state.contacts.isLoading = true;
-        state.contacts.error = null;
-      })
+
+      .addCase(addContact.pending, handlePending)
+      .addCase(addContact.rejected, handleReject)
       .addCase(addContact.fulfilled, (state, { payload }) => {
         state.contacts.items = [...state.contacts.items, payload];
         state.contacts.isLoading = false;
       })
-      .addCase(addContact.rejected, (state, { payload }) => {
-        state.contacts.error = payload;
-        state.contacts.isLoading = false;
-      })
-      .addCase(deleteContact.pending, (state, { payload }) => {
-        state.contacts.isLoading = true;
-        state.contacts.error = null;
-      })
+
+      .addCase(deleteContact.pending, handlePending)
+      .addCase(deleteContact.rejected, handleReject)
       .addCase(deleteContact.fulfilled, (state, { payload }) => {
         state.contacts.items = state.contacts.items.filter(
           contact => contact.id !== payload.id
         );
-        state.contacts.isLoading = false;
-      })
-      .addCase(deleteContact.rejected, (state, { payload }) => {
-        state.contacts.error = payload;
         state.contacts.isLoading = false;
       });
   },
